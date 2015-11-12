037305604cf92670d5efdfd912965e426f5d18b0
ahmed <ahmedbassi@gmail.com>
2015-11-12 18:24:53 +1030

    my changes to @diadara's fork of conky-clementine

--- anowplaying.py
+++ anowplaying.py
@@ -37,15 +37,36 @@ if __name__ == '__main__':
     parser.add_option('-b',  '--bitrate', action='store_true', help='bitrate of the track')
     parser.add_option('-s',  '--sample',  action='store_true', help='sample rate of the track')
     parser.add_option('-c',  '--cover',   metavar='filename',  help='copy cover art to destination file')
+    parser.add_option('-w',  '--titlewrapped',   metavar='length', help='title of the track (text-wrapped)')
+    parser.add_option('--bar',   metavar='barmaxlength', help='bar')
+    parser.add_option('--bar-remaining',   metavar='barmaxlength', help='bar')
+    parser.add_option('--albumwrapped',   metavar='maxlength', help='bar')
     
     '''Get the parser options printed'''
     (opts, args) = parser.parse_args()
     if opts.artist and amarokdict.has_key('artist') :
-        print amarokdict['artist']
+        artist = amarokdict['artist'].replace("\\", "\n").replace("/", "\n")
+        print artist
+        
     if opts.title and amarokdict.has_key('title'):
         print amarokdict['title']
+    if opts.titlewrapped and amarokdict.has_key('title'):
+        title = amarokdict['title'].replace("\\", "\n").replace("/", "\n")
+        if len(title) > 25:
+            print title + "\n" + title
+            
     if opts.album and amarokdict.has_key('album'):
         print amarokdict['album']
+        
+    if opts.albumwrapped and amarokdict.has_key('album'):
+        album = amarokdict['album']
+        if len(album) > int(opts.albumwrapped):
+            import textwrap
+            print textwrap.fill(album, int(opts.albumwrapped))
+        else:
+            print opts.albumwrapped
+            print album
+            
     if opts.genre and amarokdict.has_key('genre'):
         print amarokdict['genre']
     if opts.year and amarokdict.has_key('year'):
@@ -74,6 +95,19 @@ if __name__ == '__main__':
         print mtime
     if opts.progress and progress is not None:
         print progress
+    if opts.bar:
+        maxbarlength = int(opts.bar)
+
+        cpos    = amarok.PositionGet()/1000
+        mt      = amarokdict['mtime']/1000
+        mtime   = str(mt/60)+":"+str(mt%60) if mt%60>9 else str(mt/60)+":0"+str(mt%60)
+        etime   = str(cpos/60)+":"+str(cpos%60) if cpos%60>9 else  str(cpos/60)+":0"+str(cpos%60)
+        rtime   = str((mt-cpos)/60)+":"+str((mt-cpos)%60) if (mt-cpos)%60>9 else str((mt-cpos)/60)+":0"+str((mt-cpos)%60)
+        progress= float(cpos)/float(mt)
+        
+        bar_length = int(progress * maxbarlength)
+        
+        print("_" * bar_length)
 
     if opts.cover :
         cover = amarokdict['arturl']
